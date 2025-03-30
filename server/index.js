169aa58f67119aa7e39ea9bc29f78d0f5931cfc8
vanshikagarg1411 <gargvanshika142004@gmail.com>
2025-03-30 19:18:40 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://vanshika-garg:iluvnamjoon@mern-project.llxda.mongodb.net/mern-project?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
